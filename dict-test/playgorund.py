c0f783a71fe66f11c77e85334c14d1f540e000c4
Danny Ocaranza <docaranzan@outlook.com>
2021-04-12 16:28:17 -0400

    app done

---
+++ dict-test/playgorund.py
@@ -0,0 +1,18 @@
+lst = [1, 1, 11, 7]
+print(lst)
+lst.append(16)
+print(lst)
+lst.remove(11)
+print(lst)
+lst.sort()
+print()
+
+st = {1, 1, 11, 7}
+print(st)
+
+d = {'bob': 0,
+     'sarah': 0}
+d['bob'] += 1
+print(d)
+d['john'] = 7
+print(d)
